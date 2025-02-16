d3aee8010f4c5c3b7244fc11b4a282440bda0774
vincington3626 <kime3626@byui.edu>
2025-02-15 17:48:54 -0700

    fixed

--- recipes/js/main.js
+++ recipes/js/main.js
@@ -11,7 +11,7 @@ function tagsTemplate(tags) {
 }
 
 function ratingTemplate(rating) {
-    let html = `<span class="rating" role="img" aria-label="Rating: ${rating} out of 5 stars">`
+	let html = `<span class="rating" role="img" aria-label="Rating: ${rating} out of 5 stars">`
     for (let i = 1; i <= 5; i++) {
         if (i <= rating) {
             html += `<span aria-hidden="true" class="icon-star">⭐</span>`;
@@ -21,11 +21,11 @@ function ratingTemplate(rating) {
         }
     }
 
-    html += `</span>`;
-    return html;
+	html += `</span>`;
+	return html;
 }
 
-function recipeTemplate(recipe) {
+function recipeTemplate (recipe) {
     return `<section class="recipe">
                 <img src="${recipe.image}" alt="">
                 <div class="info">
@@ -41,39 +41,37 @@ function recipeTemplate(recipe) {
 
 function renderRecipes(recipeList) {
     const outputElement = document.querySelector('#content');
-
     const listOfRecipes = recipeList.map(recipeTemplate)
-
     outputElement.insertAdjacentHTML("beforeend", listOfRecipes);
 }
 
 function init() {
     const recipe = getRandomListEntry(recipes)
     renderRecipes([recipe]);
-}
+  }
 
 function filterRecipes(query) {
     function searchRecipes(recipe) {
-        return recipe.name.toLowerCase().includes(query) ||
-            recipe.recipeIngredient.find(ingredient => ingredient.toLowerCase().includes(query)) ||
-            recipe.description.toLowerCase().includes(query) ||
-            recipe.tags.find(tag => tag.toLowerCase().includes(query));
+        return recipe.name.toLowerCase().includes(query) || 
+        recipe.recipeIngredient.find(ingredient => ingredient.toLowerCase().includes(query)) || 
+        recipe.description.toLowerCase().includes(query) || 
+        recipe.tags.find(tag => tag.toLowerCase().includes(query)); 
     }
     return recipes.filter(searchRecipes);
 }
 
-function sortNames(a, b) {
+function sortNames(a,b) {
     if (a.name.toLowerCase() > b.name.toLowerCase()) {
-        return 1;
+      return 1;
     } else if (a.name.toLowerCase() < b.name.toLowerCase()) {
-        return -1;
+      return -1;
     }
-    return 0;
-}
+   return 0;
+  }
 
 init();
 
-document.querySelector("#search-bar").addEventListener("submit", function (event) {
+document.querySelector("#search-bar").addEventListener("submit", function(event) {
     event.preventDefault();
     let searchQuery = event.target.search.value.toLowerCase();
     event.target.search.value = "";
